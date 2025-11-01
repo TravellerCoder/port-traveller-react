b8ab8ae7ad5cb56e81c7c2a953e74c59f4e5f649
Traveller Coder <guillermobukowski@gmail.com>
2025-11-01 15:24:42 -0300

    creacion de hook de theme

---
+++ src/assets/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./footer.css"
+
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="footer">
+        <div class="footer-text">
+            <p>Traveller Coder &copy; 2024</p>
+        </div>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Hero from './assets/components/hero/Hero.jsx'
 import About from './assets/components/about/About.jsx'
 import Projects from './assets/components/projects/Projects.jsx'
 import Contact from './assets/components/contact/Contact.jsx'
+import Footer from './assets/components/footer/Footer.jsx'
 import './index.css'
   
 
@@ -15,5 +16,6 @@ createRoot(document.getElementById('root')).render(
     <About/>
     <Projects/>
     <Contact/>
+    <Footer/>
   </StrictMode>,
 )
